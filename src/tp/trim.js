317823dfbeb57824dc02b0e5f2877cc04ec9e5ba
hezongtai <1399266188@qq.com>
2016-08-01 20:31:13 +0800

    Update trim.js

--- src/tp/trim.js
+++ src/tp/trim.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const fs = require('fs')
-const os = require('os')
 const path = require('path')
 
 const async = require('async')
@@ -28,7 +27,7 @@ module.exports = (inputPath, hasAlpha, callback) => {
       resizeImages(files, cb)
     },
     (files, cb) => {
-      getTrimInfo(files, cb)
+      getCropInfo(files, cb)
     }
   ], callback)
 }
@@ -56,7 +55,7 @@ function readDir(input, hasAlpha, files, callback) {
 
 function resizeImages(files, callback){
   async.eachSeries(files, (file, next) => {
-    exec(`convert -define png:exclude-chunks=date -resize ${RESIZE} ${file.iPath} -bordercolor transparent -border 1 -trim ${file.tPath}`, (err) => {
+    exec(`convert -resize ${RESIZE} ${file.iPath} ${file.tPath}`, (err) => {
       if(err) throw err
       next()
     })
@@ -65,46 +64,6 @@ function resizeImages(files, callback){
   })
 }
 
-function getTrimInfo(input, files, callback) {
-  const filePaths = files.map(file => {
-    return '"' + file.tPath + '"'
-  })
-
-  const trimmedFiles = []
-
-  exec('identify ' + filePaths.join(' '), (err, stdout) => {
-    if (err) return callback(err)
-
-    let sizes = stdout.split('\n')
-    sizes = sizes.splice(0, sizes.length - 1)
-    sizes.forEach(item => {
-      const file = {}
-      const size = item.match(/ ([0-9]+)x([0-9]+) /)
-
-      file.x = file.y = 0
-      file.width = parseInt(size[1], 10)
-      file.height = parseInt(size[2], 10)
-
-      file.area = file.width * file.height
-      // console.log(file.area)
-
-      const rect = item.match(/ ([0-9]+)x([0-9]+)[\+\-]([0-9]+)[\+\-]([0-9]+) /)
-      file.trim = {}
-      file.trim.x = parseInt(rect[3], 10) - 1
-      file.trim.y = parseInt(rect[4], 10) - 1
-      file.trim.width = parseInt(rect[1], 10) - 2
-      file.trim.height = parseInt(rect[2], 10) - 2
-
-      file.path = item.match(/.+\.png/)[0]
-
-      trimmedFiles.push(file)
-    })
-
-    callback(null, trimmedFiles)
-  })
-}
-
-/*
 function getCropInfo(files, callback) {
     async.eachSeries(files, (file, next) => {
     // file.tPath = path.join(os.tmpDir(), `${file.name}_trimmed.png`) // temp path for trimed file
@@ -133,4 +92,4 @@ function getCropInfo(files, callback) {
   }, () => {
     callback(null, files)
   })
-}*/
+}
