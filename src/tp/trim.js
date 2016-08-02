0e91d9bc3ff5b3ea55cfd1336f312acd4b0c1403
hezongtai <1399266188@qq.com>
2016-08-02 10:48:28 +0800

    Update trim.js

--- src/tp/trim.js
+++ src/tp/trim.js
@@ -24,10 +24,10 @@ module.exports = (inputPath, hasAlpha, callback) => {
       readDir(inputPath, hasAlpha, [], cb)
     },
     (files, cb) => {
-      resizeImages(files, cb)
+      trimImages(inputPath, files, cb)
     },
     (files, cb) => {
-      getCropInfo(files, cb)
+      getTrimInfo(inputPath, files, cb)
     }
   ], callback)
 }
@@ -53,6 +53,62 @@ function readDir(input, hasAlpha, files, callback) {
   })
 }
 
+
+function trimImages(input, files, callback) {
+  async.eachSeries(files, (file, next) => {
+	// have to add 1px transparent border because imagemagick does trimming based on border pixel's color
+    // only to list the result on what part of the image was trimmed, not the actual trimmed image
+    // use alpha channel's crop area
+
+    exec(`convert -define png:exclude-chunks=date -resize ${RESIZE} ${file.iPath} -bordercolor transparent -border 1 -trim ${file.tPath}`, err => {
+      if(err) throw err
+      next()
+    })
+  }, () => {
+    callback(null, files)
+  })
+}
+
+function getTrimInfo(input, files, callback) {
+  const filePaths = files.map(file => {
+    return '"' + file.tPath + '"'
+  })
+
+  const trimmedFiles = []
+
+  exec('identify ' + filePaths.join(' '), (err, stdout) => {
+    if (err) return callback(err)
+
+    let sizes = stdout.split('\n')
+    sizes = sizes.splice(0, sizes.length - 1)
+    sizes.forEach(item => {
+      const file = {}
+      const size = item.match(/ ([0-9]+)x([0-9]+) /)
+
+      file.x = file.y = 0
+      file.width = parseInt(size[1], 10)
+      file.height = parseInt(size[2], 10)
+
+      file.area = file.width * file.height
+      // console.log(file.area)
+
+      const rect = item.match(/ ([0-9]+)x([0-9]+)[\+\-]([0-9]+)[\+\-]([0-9]+) /)
+      file.trim = {}
+      file.trim.x = parseInt(rect[3], 10) - 1
+      file.trim.y = parseInt(rect[4], 10) - 1
+      file.trim.width = parseInt(rect[1], 10) - 2
+      file.trim.height = parseInt(rect[2], 10) - 2
+
+      file.path = item.match(/.+\.png/)[0]
+
+      trimmedFiles.push(file)
+    })
+
+    callback(null, trimmedFiles)
+  })
+}
+
+/*
 function resizeImages(files, callback){
   async.eachSeries(files, (file, next) => {
     exec(`convert -resize ${RESIZE} ${file.iPath} ${file.tPath}`, (err) => {
@@ -93,3 +149,4 @@ function getCropInfo(files, callback) {
     callback(null, files)
   })
 }
+*/
