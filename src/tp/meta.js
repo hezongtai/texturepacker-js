6eb6ce94147a7fa7af1c6efa9d27a3cdfba080ba
hezongtai <1399266188@qq.com>
2016-09-07 17:12:54 +0800

    Update meta.js

--- src/tp/meta.js
+++ src/tp/meta.js
@@ -1,29 +1,41 @@
 'use strict'
 
 const fs = require('fs')
+const plist = require('plist')
 
 module.exports = (files, options, callback) => {
   const frames = {}
 
   files.forEach(file => {
     frames[file.name] = {}
-    frames[file.name].rotated = false
-    frames[file.name].trimmed = true
-    frames[file.name].frame = {x: file.x, y: file.y, w: file.width, h: file.height}
-    frames[file.name].spriteSourceSize = {x: file.trimX, y: file.trimY, w: file.width, h: file.height}
-    frames[file.name].sourceSize = {w: file.trimW, h: file.trimH}
+    // frames[file.name].rotated = false
+    // frames[file.name].trimmed = true
+    // frames[file.name].frame = {x: file.x, y: file.y, w: file.width, h: file.height}
+    // frames[file.name].spriteSourceSize = {x: file.trimX, y: file.trimY, w: file.width, h: file.height}
+    // frames[file.name].sourceSize = {w: file.trimW, h: file.trimH}
+    //
+    const offsetX = (file.width * 0.5) - ((file.trimW * 0.5) - file.trimX)
+    const offsetY = (file.height * 0.5) - ((file.trimH * 0.5) - file.trimY)
+
+    frames[file.name].aliases = []
+    frames[file.name].spriteOffset = `{${offsetX},${offsetY}}`
+    frames[file.name].spriteSize = `{${file.width},${file.height}}`
+    frames[file.name].spriteSourceSize = `{${file.trimW},${file.trimH}}`
+    frames[file.name].textureRect = `{{${file.x},${file.y}},{${file.width},${file.height}}}`
+    frames[file.name].textureRotated = false
   })
 
-  const meta = {
-    image: `${options.name}.png`,
-    format: 'RGB888',
-    size: {w: options.width, h: options.height},
-    totalFrames: files.length,
-    scale: 1
+  const metadata = {
+    premultiplyAlpha: false,
+    format: 3,
+    pixelFormat: 'RGB888',
+    textureFileName: `${options.name}.pvr`,
+    realTextureFileName: `${options.name}.pvr`,
+    size: `{${options.width},${options.height}}`
   }
 
-  const json = {frames, meta}
-  fs.writeFile(`${options.output}/${options.name}.json`, JSON.stringify(json, null, 2), err => {
+  const json = {frames, metadata}
+  fs.writeFile(`${options.output}/${options.name}.plist`, plist.build(json), err => {
     if(err) throw err
     callback()
   })
