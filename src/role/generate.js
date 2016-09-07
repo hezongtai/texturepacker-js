3bfe7550faaa34ce876d52e9a2b4aff53d03f46b
hezongtai <1399266188@qq.com>
2016-09-07 19:32:10 +0800

    Update generate.js

--- src/role/generate.js
+++ src/role/generate.js
@@ -1,32 +1,101 @@
 'use strict'
 
 const fs = require('fs')
+const path = require('path')
 
 const async = require('async')
 const ProgressBar = require('progress')
 
-const pack = require('../tp/packer')
+const pack = require('../tp/texturepacker')
+const crop = require('../tp/crop')
 
 // generate spritesheets by vo
-module.exports = function generate(name, input, output, cb) {
-  // generate body's spritesheet
+module.exports = function generate(config, cb) {
   const genList = []
+  const alphaGenList = []
 
-  fs.readdir(`${input}/${name}`, (err, files) => {
-    if(err) throw err
-    files.forEach(file => {
-      if(fs.statSync(`${input}/${name}/${file}`).isDirectory()) {
-        genList.push({iDir: `${input}/${name}/${file}`, oDir: `${output}/${name}`, name: file})
+  const input = config.input
+  const output = config.output
+  const name = config.name
+
+  const used = {}
+
+  for(const wkey in config) {
+    if(wkey === 'input' || wkey === 'output' || wkey === 'name') continue
+
+    const actions = config[wkey]
+    // console.log(actions)
+    for(const akey in actions) {
+      const action = actions[akey]
+
+      const body = action.body
+      const weapon = action.weapon
+      const deco = action.deco
+
+      // body
+      checkImages(body)
+
+      // weapon
+      checkImages(weapon)
+
+      // deco
+      checkImages(deco)
+
+      config[wkey][akey].frames = used[body]
+      config[wkey][akey].body = (/(.+)(_a)$/).exec(body)[1]
+    }
+  }
+
+  console.log(config)
+
+  function checkImages(folder) {
+    if(fs.statSync(`${input}/${name}/${folder}`).isDirectory()) {
+      if(!used[folder]) {
+        const alpha = (/(.+)(_a)$/).exec(folder)
+        if(alpha) {
+          // if it has alpha channel images already, pack them both
+          genList.push({iDir: `${input}/${name}/${folder}`, oDir: `${output}/${name}`, name: folder})
+          // genList.push({iDir: `${input}/${name}/${alpha[1]}`, oDir: `${output}/${name}`, name: alpha[1]})
+        }else{
+          // if not, make the alpha channel images first
+          alphaGenList.push({iDir: `${input}/${name}/${folder}`, oDir: `${input}/${name}/${folder}_a`, name: `${folder}_a`})
+          genList.push({iDir: `${input}/${name}/${folder}_a`, oDir: `${output}/${name}`, name: `${folder}_a`})
+        }
+
+        used[folder] = findImages(fs.readdirSync(`${input}/${name}/${folder}`))
       }
+    }else{
+      throw new Error(`Folder not exist:${folder}`)
+    }
+  }
+
+  function findImages(images) {
+    const files = []
+    images.forEach(image => {
+      if(path.extname(image).toLowerCase() === '.png') files.push(path.basename(image, '.png'))
     })
 
-    const bar = new ProgressBar('Packing sprites: [:bar] :current/:total :input -> :output', {
-      total: genList.length
-    })
+    return files
+  }
 
-    async.eachSeries(genList, (io, next) => {
-      pack(io.iDir, {output: io.oDir, name: io.name}, next)
-      bar.tick({input: io.iDir, output: io.oDir})
-    }, cb)
+  // now pack the images
+  const bar = new ProgressBar('Packing sprites: [:bar] :current/:total :input -> :output', {
+    total: genList.length
   })
+
+  async.waterfall([
+    cb => {
+      async.eachSeries(genList, (io, next) => {
+        pack(io.iDir, {output: io.oDir, character: name, name: io.name, genAlpha: false}, next)
+        bar.tick({input: io.iDir, output: io.oDir})
+      }, cb)
+    },
+    cb => {
+      crop(genList, cb)
+    },
+    cb => {
+      fs.writeFileSync(`${output}/${name}/config.json`, JSON.stringify(config, null, 2))
+      cb()
+    }
+  ])
 }
