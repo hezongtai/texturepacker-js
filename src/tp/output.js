bf14fc0fc16c61e387d52da69e8e7153343d3925
hezongtai <1399266188@qq.com>
2016-09-08 04:38:26 -0400

    fix dir

--- src/tp/output.js
+++ src/tp/output.js
@@ -5,7 +5,7 @@ const path = require('path')
 
 const exec = require('platform-command').exec
 
-const PVRTexTool = '/Applications/Imagination/PowerVR_Graphics/PowerVR_Tools/PVRTexTool/CLI/OSX_x86/PVRTexToolCLI'
+// const PVRTexTool = '/Applications/Imagination/PowerVR_Graphics/PowerVR_Tools/PVRTexTool/CLI/OSX_x86/PVRTexToolCLI'
 
 module.exports = (input, options, files, callback) => {
   // POT
@@ -38,14 +38,14 @@ module.exports = (input, options, files, callback) => {
       // delete alpha channel
       command.push(`&& convert ${options.output}/${options.name}_a.png -background black -alpha remove ${options.output}/${options.name}_a.png`)
       // convert to pvr
-      command.push(`&& ${PVRTexTool} -i ${options.output}/${options.name}_a.png -f PVRTC1_4,UBN,lRGB -o ${options.output}/${options.name}_a.pvr`)
+      // command.push(`&& ${PVRTexTool} -i ${options.output}/${options.name}_a.png -f PVRTC1_4,UBN,lRGB -o ${options.output}/${options.name}_a.pvr`)
     }
   }
   // remove alpha channel from origin
   command.push(`&& convert ${options.output}/${options.name}.png -background black -alpha remove ${options.output}/${options.name}.png`)
 
   // convert to pvr
-  command.push(`&& ${PVRTexTool} -i ${options.output}/${options.name}.png -f PVRTC1_4,UBN,lRGB -o ${options.output}/${options.name}.pvr`)
+  // command.push(`&& ${PVRTexTool} -i ${options.output}/${options.name}.png -f PVRTC1_4,UBN,lRGB -o ${options.output}/${options.name}.pvr`)
 
   files.forEach(file => {
     // create trim frame
