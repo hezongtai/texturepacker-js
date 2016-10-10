5cecdcf37ca84dd3058c0c7fcab698bbaa0ba133
hezongtai <1399266188@qq.com>
2016-10-10 06:08:35 -0400

    change packer and trim

--- src/tp/packer.js
+++ src/tp/packer.js
@@ -2,9 +2,6 @@
 const async = require('async')
 
 const trim = require('./trim')
-const bin = require('./bin')
-const output = require('./output')
-const meta = require('./meta')
 
 /**
  * walk through all png in the input folder, and pack them into one png
@@ -17,17 +14,6 @@ module.exports = function texturepack(input, options, callback) {
   async.waterfall([
     function (cb) {
       trim(input, options.hasAlpha, cb)
-    },
-    function (files, cb) {
-      bin(files, cb)
-    },
-    function (files, width, height, cb) {
-      options.width = width
-      options.height = height
-      output(input, options, files, cb)
-    },
-    function (files, cb) {
-      meta(files, options, cb)
     }
   ], callback)
 }
